f090fa7c5e506088ab5eaa0a3b279063534cab42
SrjAdhikari <surajadhikari055@gmail.com>
2024-12-07 22:15:01 +0900

    Day-11 Learn Prototype Chaining In JavaScript

--- 03_JavaScript/Day-11/Prototype_Chaining.js
+++ 03_JavaScript/Day-11/Prototype_Chaining.js
@@ -44,3 +44,73 @@ console.log(arr.__proto__.__proto__.__proto__ === null);                // true
 console.log(Object.prototype.__proto__ === null);                       // true
 
 //* The Chain : arr -> Array.prototype -> Object.prototype -> null
+
+
+//* ****************************************************
+//* Custom Prototype Chaining
+//* ****************************************************
+
+//* 1. Using Object.create()
+// Define the base object `person` containing shared properties
+const person = {
+    name: "Suraj",
+    age: 27,
+    gender: "male"
+};
+
+// Create the `student` object and set `person` as its prototype
+const student = Object.create(person);
+student.faculty = "Computer Science";
+student.year = 3;
+
+// Create the `teacher` object and set `person` as its prototype
+const teacher = Object.create(person);
+teacher.subject = "mathematics";
+teacher.experience = 5;
+teacher.salary = 500000;
+
+// Access inherited properties from the prototype `person`
+console.log(student.age);                           // Output: 27 (inherited from `person`)
+console.log(student.gender);                        // Output: male (inherited from `person`)
+console.log(teacher.name);                          // Output: Suraj (inherited from `person`)
+console.log(teacher.gender);                        // Output: male (inherited from `person`)
+
+// Verify that the prototype of `student` and `teacher` is `person`
+console.log(student.__proto__ == person);           // true
+console.log(teacher.__proto__ == person);           // true
+
+
+//* 2. Manually Setting __proto__
+// Define the base object `baseAnimal` with shared properties
+const baseAnimal = {
+    species: "Unknown",
+    legs: 4,
+    hasTail: true
+};
+
+// Define the `dog` object with specific properties
+const dog = {
+    name: "Buddy",
+    bread: "German Shepherd",
+}
+
+// Define the `cat` object with specific properties
+const cat = {
+    name: "Whiskers",
+    breed: "Siamese"
+};
+
+// Set the prototype of `dog` and `cat` to `baseAnimal`
+dog.__proto__ = baseAnimal;
+cat.__proto__ = baseAnimal;
+
+// Access inherited properties from `baseAnimal` through `dog` and `cat`
+console.log(dog.species);
+console.log(dog.legs);
+console.log(cat.hasTail);
+console.log(cat.name);
+console.log(dog.bread);
+
+// Verify that the prototype of `dog` and `cat` is `baseAnimal`
+console.log(dog.__proto__ === baseAnimal);          // Output: true (prototype of `dog` is `baseAnimal`)
+console.log(cat.__proto__ === baseAnimal);          // Output: true (prototype of `cat` is `baseAnimal`)    
