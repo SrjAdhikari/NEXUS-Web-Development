1818b8037a0f460884fba2821ffcd38840cf0307
SrjAdhikari <surajadhikari055@gmail.com>
2025-02-11 09:14:16 +0900

    Add clothingItems dataset with product details

---
+++ 06_React/07_Myntra_Project/src/dummy-data.js
@@ -0,0 +1,94 @@
+const clothingItems = [
+    {
+        img: "https://m.media-amazon.com/images/I/91V12ufIXRL._AC_UY1100_.jpg",
+        name: "Shirt",
+        discount: { min: 20, max: 50 },
+        price: 49
+    },
+    {
+        img: "https://img.freepik.com/free-photo/clean-blank-shirt-hanger_23-2149347514.jpg",
+        name: "T-Shirt",
+        discount: { min: 30, max: 60 },
+        price: 39
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiyzyTLSuzpYM5FvXa9XdMu2cm39Dl_a5U1g&s",
+        name: "Leather Jacket",
+        discount: { min: 40, max: 80 },
+        price: 99
+    },
+    {
+        img: "https://img.freepik.com/free-photo/sport-outdoor-woman-shoes_144627-45943.jpg",
+        name: "Running Shoes",
+        discount: { min: 60, max: 90 },
+        price: 89
+    },
+    {
+        img: "https://img.freepik.com/premium-photo/front-view-woman-with-headphones_926199-4397118.jpg",
+        name: "Warm Hoodie",
+        discount: { min: 70, max: 80 },
+        price: 55
+    },
+    {
+        img: "https://img.freepik.com/free-photo/man-shorts-summer-fashion-photoshoot-close-up_53876-108600.jpg",
+        name: "Cotton Shorts",
+        discount: { min: 5, max: 30 },
+        price: 29
+    },
+    {
+        img: "https://img.freepik.com/free-photo/pop-party-studio-with-legs-wearing-heels_23-2149403565.jpg",
+        name: "Ankle Socks",
+        discount: { min: 2, max: 10 },
+        price: 19
+    },
+    {
+        img: "https://img.freepik.com/free-photo/warm-cozy-sweaters_169016-3086.jpg",
+        name: "Wool Sweater",
+        discount: { min: 15, max: 40 },
+        price: 40
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVzE9RYxsTra4VC-ChH6QHcJIRhP2FbJ93XQ&s",
+        name: "Formal Suit",
+        discount: { min: 25, max: 50 },
+        price: 149
+    },
+    {
+        img: "https://img.freepik.com/free-photo/medium-shot-smiley-woman-winter-season_23-2149248954.jpg",
+        name: "Winter Coat",
+        discount: { min: 35, max: 60 },
+        price: 79
+    },
+    {
+        img: "https://img.freepik.com/free-photo/fashion-girl-standing-summer-park_1157-23457.jpg",
+        name: "Knitted Cardigan",
+        discount: { min: 15, max: 30 },
+        price: 59
+    },
+    {
+        img: "https://img.freepik.com/free-photo/view-trucker-hat-with-baseball_23-2149410091.jpg",
+        name: "Baseball Cap",
+        discount: { min: 5, max: 10 },
+        price: 25
+    },
+    {
+        img: "https://img.freepik.com/free-photo/red-winter-knitted-bobble-hat-scarf-gloves-isolated-white-background_1101-2382.jpg",
+        name: "Winter Gloves",
+        discount: { min: 10, max: 20 },
+        price: 35
+    },
+    {
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6ns2it1GR8XbMwrxhNvakcJlv2RvmmAlJCQ&s",
+        name: "Sneakers",
+        discount: { min: 15, max: 30 },
+        price: 99
+    },
+    {
+        img: "https://i.ebayimg.com/images/g/AsoAAOSw5OtjRX3u/s-l1200.jpg",
+        name: "Business shoes",
+        discount: { min: 20, max: 40 },
+        price: 115
+    }
+];
+
+export default clothingItems;
