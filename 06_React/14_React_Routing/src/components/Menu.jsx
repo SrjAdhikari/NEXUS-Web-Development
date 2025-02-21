ac823f6f788423aa0365f19d54e7be6ce81e3dd9
SrjAdhikari <surajadhikari055@gmail.com>
2025-02-21 16:01:16 +0900

    Add Menu page with food items and prices

---
+++ 06_React/14_React_Routing/src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <div>
+            <ul>
+                <li>Pizza - $10</li>
+                <li>Burger - $8</li>
+                <li>Pasta - $12</li>
+                <li>Salad - $6</li>
+                <li>Ice Cream - $4</li>
+                <li>Drinks - $2</li>
+                <li>Dessert - $3</li>
+                <li>Snacks - $1</li>
+                <li>Soup - $5</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu
