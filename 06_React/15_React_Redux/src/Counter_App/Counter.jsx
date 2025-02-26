1c944f32086263b21958db10b37b4aa7727a6c1e
SrjAdhikari <surajadhikari055@gmail.com>
2025-02-26 12:33:58 +0900

    Add Counter component with Redux state integration

---
+++ 06_React/15_React_Redux/src/Counter_App/Counter.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './Slice';
+
+const Counter = () => {
+    // Get the current count value from the Redux store
+    const count = useSelector((state) => state.counter.count);
+
+    // Get the dispatch function to send actions to the Redux store
+    const dispatch = useDispatch();
+
+    return (
+        <div className="counter-container">
+            <p>Count is : {count}</p>
+            <div className="buttons">
+                {/* Dispatch the increment action when the button is clicked */}
+                <button onClick={() => dispatch(increment())}>Increment</button>
+
+                {/* Dispatch the decrement action when the button is clicked */}
+                <button onClick={() => dispatch(decrement())}>Decrement</button>
+
+                {/* Dispatch the reset action when the button is clicked */}
+                <button onClick={() => dispatch(reset())}>Reset</button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
